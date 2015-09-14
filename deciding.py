dad1c454db8c4e92c37164d485e5176ec4aec5f3
Hayden Brannon <haydenbrannon@Alveyworld-30.local>
2015-09-14 10:18:17 -0600

    Even more things

---
+++ deciding.py
@@ -0,0 +1,7 @@
+#Learning about if statements
+
+number = int(raw_input("Enter a number: " ))
+if number = < 100:
+	print = "The number is less than 100"
+if number = > 100:
+	print = "The number is more then 100"
