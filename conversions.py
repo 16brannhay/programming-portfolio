2c7d2cc9ce9ea6bd41c5db69bcc88676c26b20ae
Hayden Brannon <haydenbrannon@Alveyworld-30.local>
2015-09-14 09:47:57 -0600

    More things

---
+++ conversions.py
@@ -0,0 +1,6 @@
+float(string) -> float value
+int(string) -> integer value
+str(integer) or str(float) -> string representation
+>>> a = 8.126768
+>>> str(a)
+'8.126768'
---
+++ learningpython.py
@@ -0,0 +1,5 @@
+# learning Python
+print "Hello, World!"
+number = int(raw_input("Enter an integer: "))
+if number < 100:
+print "10"
