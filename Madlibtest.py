7141dcf21d90bb90224a52d710d79858600047a0
Hayden Brannon <haydenbrannon@Alveyworld-30.local>
2016-01-08 09:58:43 -0700

    Madlib

---
+++ Madlibtest.py
@@ -0,0 +1,9 @@
+print "It was a"
+adjective = raw_input("Enter adjective")
+print "It was a" + adjective
+print ", cold November day. I woke up to the"
+adjective2 = raw_input("Enter adjective")
+print "cold November day. I woke up to the" + adjective2
+print "smell of"
+bird = raw_input("type of bird")
+print "smell of" + bird
